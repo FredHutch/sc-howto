a7622afaa462d5faecaf6deceb9020827946ef7f
Dirk Petersen <dirkpetersen@users.noreply.github.com>
2017-12-26 10:22:42 -0800

    cleanup work

--- samba/usr/local/sbin/join-ad.sh
+++ samba/usr/local/sbin/join-ad.sh
@@ -2,12 +2,16 @@
 
 hostfqdn=$(hostname -f)
 computername=$(hostname -s)
-OU='ou=Servers,ou=Computers,ou=SciComp'
+OU='ou=Computers' # or something 'ou=Computers,ou=mydvision'
 
-if [[ "$hostfqdn" == "$computername" ]]; then 
+if [[ "$hostfqdn" == "$computername" ]]; then
   echo "hostname -f and hostname -s return the same value, error in /etc/hosts"
   exit
 fi
 
-msktutil.1404 --dont-expire-password --no-pac --computer-name $computername --server dc --enctypes 0x07 -b "$OU" -k /etc/krb5.keytab -h $hostfqdn -s host/$hostfqdn -s HOST/$computername --upn host/$hostfqdn  --description "Kerberos Account by msktutil" --set-samba-secret
+echo "host ${hostfqdn} joining domain in OU ${OU} ..."
 
+msktutil --dont-expire-password --no-pac --computer-name $computername \
+         --server dc --enctypes 0x07 -b "$OU" -k /etc/krb5.keytab -h $hostfqdn \
+         -s host/$hostfqdn -s HOST/$computername --upn host/$hostfqdn  \
+         --description "Kerberos Account for SAMBA by msktutil" --set-samba-secret
